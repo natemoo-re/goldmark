4412414409e290ee50bf47a5a7bc45f6337fc058
Nate Moore <nate@skypack.dev>
2021-09-19 22:20:01 -0500

    chore: update benchmark

--- bench/mod.ts
+++ bench/mod.ts
@@ -1,15 +1,20 @@
-import { transform } from "https://deno.land/x/goldmark/mod.ts";
+import ms from 'https://cdn.skypack.dev/ms';
+import { init, transform } from "../deno/mod.ts";
 
-const runs = 5000;
+await init();
+const runs = 100000;
 const times: number[] = [];
 const content = await Deno.readTextFile(new URL('./content.md', import.meta.url));
 
+const start = performance.now();
 for (let i = 0; i < runs; i++) {
   const start = performance.now();
   await transform(content)
   const end = performance.now();
   times.push(end - start)
 }
-
-console.log(times[0])
-console.log(times.slice(1).reduce((avg, ent) => avg + ent, 0) / runs)
+const end = performance.now();
+const duration = end - start;
+const avg = times.reduce((avg, ent) => avg + ent, 0) / runs;
+console.log(`\nCompleted ${runs} runs in ${ms(duration)}`);
+console.log(`Average run ${ms(Math.floor(avg * 100) / 100)}`)
